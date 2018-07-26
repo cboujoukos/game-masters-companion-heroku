0141366170e8e6ac0c82cd0aaa46733b5a8e3503
Chris Boujoukos <github email address>
2018-07-26 12:01:53 -0400

    bundled and added .env

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125192503) do
+ActiveRecord::Schema.define(version: 2018_01_25_192503) do
 
   create_table "attacks", force: :cascade do |t|
     t.string "name"
